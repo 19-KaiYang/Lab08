b6add6e8131f42c13bdece5d68e38b52d4212d7b
11-Vanness <114893002+19-KaiYang@users.noreply.github.com>
2022-10-26 10:59:27 +0800

    Player only move within scene

--- 19_KaiYang_Lab08/Assets/Scripts/Player.cs
+++ 19_KaiYang_Lab08/Assets/Scripts/Player.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
     public float speed;
+
+    
     // Start is called before the first frame update
     void Start()
     {
@@ -17,9 +19,22 @@ public class Player : MonoBehaviour
 
         float verticalInput = Input.GetAxis("Vertical");
 
-        transform.position = transform.position + new Vector3(0 , verticalInput * speed * Time.deltaTime, 0);
+        transform.position = transform.position + new Vector3(0, verticalInput * speed * Time.deltaTime, 0);
 
-      
+        if (transform.position.y >= 3.95f)
+        {
+            transform.position = new Vector3(transform.position.x, 3.95f, transform.position.z);
+           
+        }
+        if (transform.position.y <= -3.95f)
+        {
+
+            transform.position = new Vector3(transform.position.x, -3.95f, transform.position.z);
+        }
+
+
+
+
 
     }
 }
